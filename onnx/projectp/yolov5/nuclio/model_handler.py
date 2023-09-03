90e38766b45753e86daede6861b1fd986d5d68d5
Vladimir Valeyev <ValV@users.noreply.github.com>
2023-09-04 01:50:06 +0300

    Add model wrappers

---
+++ onnx/projectp/yolov5/nuclio/model_handler.py
@@ -0,0 +1,59 @@
+# Copyright (C) 2023 CVAT.ai Corporation
+#
+# SPDX-License-Identifier: MIT
+
+import cv2
+import numpy as np
+import onnxruntime as ort
+
+from projectp.inference import InferenceONNX
+
+
+class ModelHandler:
+    def __init__(self, labels):
+        self.model = None
+        self.load_network(model="yolov7-nms-640.onnx")
+        self.labels = labels
+
+    def load_network(self, model):
+        device = ort.get_device()
+        cuda = True if device == 'GPU' else False
+        try:
+            providers = ['CUDAExecutionProvider', 'CPUExecutionProvider'] if cuda else ['CPUExecutionProvider']
+            so = ort.SessionOptions()
+            so.log_severity_level = 3
+
+            self.model = InferenceONNX(model, providers=providers, sess_options=so)
+            # self.output_details = [i.name for i in self.model.get_outputs()]
+            # self.input_details = [i.name for i in self.model.get_inputs()]
+
+            self.is_inititated = True
+        except Exception as e:
+            raise Exception(f"Cannot load model {model}: {e}")
+
+    def infer(self, image, threshold):
+        image = np.array(image)  # PIL -> numpy
+        image = image[:, :, ::-1].copy()  # RGB -> BGR
+        h, w, _ = image.shape
+        detections, _, _ = self.model.prosess_image(image, confidence=threshold, save=False)
+
+        results = []
+        if detections:
+            boxes = detections[:, 1:5]
+            labels = detections[:, 6]
+            scores = detections[:, 5]
+
+            for label, score, box in zip(labels, scores, boxes):
+                xtl = max(int(box[0]), 0)
+                ytl = max(int(box[1]), 0)
+                xbr = min(int(box[2]), w)
+                ybr = min(int(box[3]), h)
+
+                results.append({
+                    "confidence": str(score),
+                    "label": self.labels.get(label, "unknown"),
+                    "points": [xtl, ytl, xbr, ybr],
+                    "type": "rectangle",
+                })
+
+        return results
